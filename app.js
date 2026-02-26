a726305f99d0be88c071bdf6f51d51767004a67a
b.jehanno <b.jehanno@criteo.com>
2026-02-26 16:37:31 +0100

    gtm+crto

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+// Criteo OneTag event via dataLayer
+window.dataLayer = window.dataLayer || [];
+window.dataLayer.push({
+  event: "crto_homepage",
+  crto: {
+    email: "",
+    siteType: "d",
+    account: 12345 // Replace with your Criteo Partner ID
+  }
+});
+
 if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("sw.js");
 }
